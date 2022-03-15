a8dd5f9cfefbbca447fddf4ad7b74e015b327eff
flansel <fjansell@gmail.com>
2022-03-15 06:57:14 -0400

    tabs

--- network/network.cpp
+++ network/network.cpp
@@ -23,12 +23,12 @@ using  std::cout, std::endl, std::vector, std::string;
  */
 int send_message(int sock, string message)
 {
-    if(send(sock, message.c_str(), message.length(), 0) < 0)
-    {
-        return 0;
-    }
+		if(send(sock, message.c_str(), message.length(), 0) < 0)
+		{
+				return 0;
+		}
 
-    return 1;
+		return 1;
 }
 
 /*
@@ -37,13 +37,13 @@ int send_message(int sock, string message)
  */
 string receive_message(int sock)
 {
-    string ret;
-    int bytes;
-    vector<char> buffer(BUF_MAXL);
+	string ret;
+	int bytes;
+	vector<char> buffer(BUF_MAXL);
 
-    do  
-    {
-        bytes = recv(sock, &buffer[0], buffer.size(), 0); 
+	do
+	{
+		bytes = recv(sock, &buffer[0], buffer.size(), 0); 
 		
 		for(char c : buffer)
 		{
@@ -51,9 +51,9 @@ string receive_message(int sock)
 				break;
 			ret += c;
 		}
-    }   
-    while(bytes == BUF_MAXL);
-    return ret;
+	}
+	while(bytes == BUF_MAXL);
+	return ret;
 }
 
 /*
@@ -61,15 +61,15 @@ string receive_message(int sock)
  */
 int create_connection_tcp(int port, string addr)
 {
-    int sock = 0;
-    struct sockaddr_in address;
+	int sock = 0;
+	struct sockaddr_in address;
 	struct hostent* server;
 
-    if((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0)
-    {
-        cout << "An Error occured while trying to create the socket" << endl;
-        exit(EXIT_FAILURE);
-    }
+	if((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+	{
+		cout << "An Error occured while trying to create the socket" << endl;
+		exit(EXIT_FAILURE);
+	}
 	
 	server = gethostbyname(addr.c_str());
 
@@ -80,17 +80,17 @@ int create_connection_tcp(int port, string addr)
 	}
 
 	bzero((char*)&address, sizeof(address));
-    address.sin_family = AF_INET;
+	address.sin_family = AF_INET;
 	bcopy((char*)server->h_addr, (char*)&address.sin_addr.s_addr, server->h_length);
-    address.sin_port = htons(port);
+	address.sin_port = htons(port);
 
-    if(connect(sock, (struct sockaddr*)&address, sizeof(address)) < 0)
-    {
-        perror("Connection");
+	if(connect(sock, (struct sockaddr*)&address, sizeof(address)) < 0)
+	{
+		perror("Connection");
 		exit(EXIT_FAILURE);
-    }
+	}
 
-    return sock;
+	return sock;
 }
 
 /*
@@ -99,37 +99,37 @@ int create_connection_tcp(int port, string addr)
  */
 int server_open_connection_tcp(int port, struct sockaddr_in* server_address)
 {
-    int sock, opt=1;
-    struct sockaddr_in address;
+	int sock, opt=1;
+	struct sockaddr_in address;
 
 
-    if((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0)
-    {
-        cout << "An error occured while creating the socket" << endl;
-        exit(EXIT_FAILURE);
-    }
+	if((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+	{
+		cout << "An error occured while creating the socket" << endl;
+		exit(EXIT_FAILURE);
+	}
 
-    if(setsockopt(sock, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt)))
-    {
-        cout << "An error occured while setting socket options" << endl;
-        exit(EXIT_FAILURE);
-    }
+		if(setsockopt(sock, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt)))
+		{
+				cout << "An error occured while setting socket options" << endl;
+				exit(EXIT_FAILURE);
+		}
 
-    address.sin_family = AF_INET;
-    address.sin_port = htons(port);
-    address.sin_addr.s_addr = INADDR_ANY;
+		address.sin_family = AF_INET;
+		address.sin_port = htons(port);
+		address.sin_addr.s_addr = INADDR_ANY;
 
-    if(bind(sock, (struct sockaddr*)&address, sizeof(address)) < 0)
-    {
-        cout << "An error occured while binding the socket" << endl;
-        exit(EXIT_FAILURE);
-    }
+		if(bind(sock, (struct sockaddr*)&address, sizeof(address)) < 0)
+		{
+				cout << "An error occured while binding the socket" << endl;
+				exit(EXIT_FAILURE);
+		}
 
-    if(listen(sock, 10) < 0)
-    {
-        cout << "Error while listening" << endl;
-        exit(EXIT_FAILURE);
-    }
+		if(listen(sock, 10) < 0)
+		{
+				cout << "Error while listening" << endl;
+				exit(EXIT_FAILURE);
+		}
 
 	memcpy(server_address, &address, sizeof(struct sockaddr_in));
 	
@@ -138,20 +138,20 @@ int server_open_connection_tcp(int port, struct sockaddr_in* server_address)
 
 int server_accept_connection_tcp(int sock, struct sockaddr_in* address, struct sockaddr_in* client_inf)
 {
-    int addr_len = sizeof(*address);   
+	int addr_len = sizeof(*address);
 	int new_socket;
 	if((new_socket = accept(sock, (struct sockaddr*)&address, (socklen_t*)&addr_len)) < 0)
-    {
-        cout << "Error while accepting" << endl;
-        exit(EXIT_FAILURE);
-    }
+		{
+				cout << "Error while accepting" << endl;
+				exit(EXIT_FAILURE);
+		}
 
 	if(client_inf != NULL)
 	{
 		memcpy(client_inf, &address, sizeof(struct sockaddr_in));
 	}
 
-    return new_socket;
+		return new_socket;
 
 }
 
